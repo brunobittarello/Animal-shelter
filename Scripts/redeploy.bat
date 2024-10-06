e1c0a86ab8dd4379a7d65f69ae1cdc01551f1a8c
Bruno Bittarello <brunobitta@hotmail.com>
2024-10-06 09:12:43 -0300

    Search prototype

--- Scripts/redeploy.bat
+++ Scripts/redeploy.bat
@@ -1,3 +1,6 @@
-docker cp sosnazario-api/. animal-shelter-api-1:sosnazario-api/.
-docker exec -it animal-shelter-api-1 mvn clean install
-docker restart  animal-shelter-api-1
+docker cp sosnazario-ui/. animal-shelter-ui-1:app/.
+docker restart  animal-shelter-ui-1
+
+@REM docker cp sosnazario-api/. animal-shelter-api-1:sosnazario-api/.
+@REM docker exec -it animal-shelter-api-1 mvn clean install
+@REM docker restart  animal-shelter-api-1
--- sosnazario-ui/app.py
+++ sosnazario-ui/app.py
@@ -2,9 +2,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class SearchFields:
+	def __init__(self, fields):
+		self.fields = fields
+
+class Selection:
+	def __init__(self, id, name, options=list()):
+		self.id = id
+		self.name = name
+		self.options = options
+
+class Option:
+	def __init__(self, value, name):
+		self.value = value
+		self.name = name
+
 @app.route('/')
-def hello():
+def index():
 	return render_template("index.html")
+
+# https://imasters.com.br/desenvolvimento/conhecendo-o-jinja2-um-mecanismo-para-templates-no-flask
+@app.route('/search')
+@app.route('/buscar')
+def search():
+	ages = Selection("age", "Ano", [Option(2020,2020),Option(2021,2021),Option(2022,2022)])
+
+	types = Selection("type", "Tipo", [Option(1,"Cães"),Option(2,"Gatos")])
+
+	genres = Selection("genre", "Gênero", [Option(1,"Femea"),Option(2,"Macho")])
+
+	size = Selection("size", "Porte", [Option(1,"P"),Option(2,"M"),Option(3,"G")])
+
+	page = SearchFields([ages, types, genres, size])
+	return render_template("search.html", page=page)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
